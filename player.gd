45eb31c6f494f70bdfd3b1f25eb679aadd0b4a6d
Mart <me@m4rt.nl>
2026-09-21 23:08:08 +0200

    feat: refine player

--- player.gd
+++ player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
 
+var START_POS = position
+
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -11,11 +13,19 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("reset"):
+		position = START_POS
 
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if direction < 0:
+		$Sprite2D.flip_h = false
+	elif direction > 0:
+		$Sprite2D.flip_h = true
 
 	move_and_slide()
